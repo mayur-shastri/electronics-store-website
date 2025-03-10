5e99b6e3bc0c79d9d63aec01939a699b120a9ab5
Mau_2993 <shastrimayur2993@gmail.com>
2025-03-10 13:25:00 +0530

    auth checking middleware created

---
+++ backend/middleware/checkAuth.js
@@ -0,0 +1,20 @@
+// check auth function to prevent unauthenticated users from accessing certain routes
+
+const jwt = require("jsonwebtoken");
+
+const checkAuth = (req, res, next) => {
+  const token = req.cookies.userToken;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ error: "Unauthorized" });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+};
+
+module.exports = checkAuth;
--- backend/routes/apis.js
+++ backend/routes/apis.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const productController = require("../controller/productController");
+const checkAuth = require("../middleware/checkAuth");
 
 const router = express.Router();
 
@@ -11,5 +12,59 @@ router.post("/add-product", productController.addProduct);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
+
+// Order Routes
+
+// place order
+router.post("/place-order", checkAuth, (req, res, next) => {
+    Order.create(req.body)
+        .then(() => {
+            res.send({
+                message: "Order Placed Successfully",
+                order: req.body,
+            });
+        })
+        .catch((error) => {
+            res.status(400).send({
+                message: "Order Failed",
+            });
+        });
+});
+
+router.post("/cancel-order", checkAuth, (req, res, next) => {
+    Order.findByIdAndUpdate(req.body.order_id, {
+        order_cancelled: true,
+        percentage_complete: 0,
+        expected_delivery_date: "",
+    })
+        .then(() => {
+            res.status(200).send({
+                message: "Order cancelled successfully",
+                order: req.body.order_id,
+            });
+        })
+        .catch(() => {
+            res.status(400).send({
+                message: "Order cancellation failed",
+            });
+        });
+});
+
+// get orders where user id matches
+router.get("/get-orders/:id", checkAuth, (req, res, next) => {
+    Order.find({ user_id: req.params.id })
+        .then((orders) => {
+            res.status(200).send({
+                orders: orders,
+                message: "Orders Fetched Successfully",
+            });
+        })
+        .catch((error) => {
+            res.status(400).send({
+                message: "Orders not found",
+            });
+        });
+});
+
 
 module.exports = router;
